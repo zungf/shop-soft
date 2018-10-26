47a7c6f09505387ff2c4d3b5f8290215626f03f2
zungf <41270843+zungf@users.noreply.github.com>
2018-10-27 00:03:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -156,33 +156,7 @@ client.on('message', message => {
 
 
 });
-var prefix = "-";
-client.on('message', message => {
-    if(message.content == prefix + 'server') {
-        var servername = message.guild.name
-        var اونر = message.guild.owner
-        var اعضاء = message.guild.memberCount
-        var ايدي = message.guild.id
-        var بلدالسيرفر = message.guild.region
-        var الرومات = message.guild.channels.size
-        var الرتب = message.guild.roles
-        var عمل = message.guild.createdAt
-        var الروم = message.guild.defaultChannel
-        var server = new Discord.RichEmbed()
-        .setThumbnail(message.guild.iconURL)
-        .addField('اسم السيرفر', servername)
-        .addField('اي دي السيرفر ' , [ايدي])
-        .addField('أعضاء السيرفر', اعضاء)
-        .addField('رومات السيرفر', الرومات)
-        .addField('روم الشات الأساسي', الروم)
-        .addField('صاحب السيرفر', اونر)
-        .addField('بلد السيرفر', بلدالسيرفر)
-        .addField('تاريخ افتتاح السيرفر', عمل)
-        .setColor('RANDOM')
 
-        message.channel.sendEmbed(server)
-    }
-});
 
 
 client.on('ready', () => {
